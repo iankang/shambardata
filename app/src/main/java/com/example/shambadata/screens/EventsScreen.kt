53dc4768bc09fcf83215a6c8a2d968c8629cd203
ian <ianngech@gmail.com>
2022-06-24 02:20:36 +0300

    edited event screen

--- app/src/main/java/com/example/shambadata/screens/EventsScreen.kt
+++ app/src/main/java/com/example/shambadata/screens/EventsScreen.kt
@@ -3,19 +3,19 @@ package com.example.shambadata.screens
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.shambadata.models.Schedule
 import com.example.shambadata.ui.theme.ShambaDataTheme
 import com.example.shambadata.viewmodels.EventsViewModel
@@ -28,7 +28,7 @@ fun EventsScreen(
     Log.e("scheduleList", eventsViewModel.scheduleList.toString())
     LazyColumn {
         items(items = eventsViewModel.scheduleList, itemContent = {
-            EventItem(
+            EventItemCard(
                 it
             )
         })
@@ -36,9 +36,9 @@ fun EventsScreen(
 }
 
 @Composable
-@Preview(name = "day", uiMode = UI_MODE_NIGHT_NO)
-@Preview(name = "night", uiMode = UI_MODE_NIGHT_YES)
-fun EventItem() {
+//@Preview(name = "day", uiMode = UI_MODE_NIGHT_NO)
+//@Preview(name = "night", uiMode = UI_MODE_NIGHT_YES)
+fun EventItemCard(schedule: Schedule) {
     ShambaDataTheme {
         Card(
             modifier = Modifier
@@ -46,16 +46,20 @@ fun EventItem() {
                 .padding(8.dp),
             backgroundColor = MaterialTheme.colors.background
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
+            Column(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(horizontal = 12.dp),
+                horizontalAlignment = Alignment.Start
             ) {
                 Text(
-                    text = "vaccination"
+                    text = schedule.scheduleActivityName!!,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+
                 )
                 
                 Text(
-                    text = "25/07/2022"
+                    text = schedule?.scheduleDate!!
                 )
             }
         }
